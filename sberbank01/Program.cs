a8d79850c0a8fde9b13fa310ae583d8a47520f4e
bul-bash <bulbash@vivaldi.net>
2018-05-27 16:33:39 +0300

    add project

---
+++ sberbank01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace sberbank01
+{
+
+    public class Program
+    {
+        static FirefoxDriver firefox;
+
+        public static void Main()
+        {
+            firefox = new FirefoxDriver(FirefoxDriverService.CreateDefaultService($"../../../"));
+            var request = "Логотип Сбербанка";
+            firefox.Navigate().GoToUrl("http://www.yandex.ru/");
+            firefox.FindElementByClassName("input__control").SendKeys(request);
+            firefox.FindElementByClassName("input__control").SendKeys(Keys.Enter);
+            firefox.Navigate().GoToUrl("https://yandex.ru/images/search?text="+request);
+
+            GetCountPic("Сбербанк");
+            GetCountPic("Сбербанк12345");
+            Console.ReadKey();
+            firefox.Quit();
+        }
+
+        public static int GetCountPic(string alt)
+        {
+            var imagesCollection = firefox.FindElements(By.TagName("img"));
+            var sberImages = imagesCollection.Where(webElement => webElement.GetAttribute("alt").Contains(alt));
+            var count = sberImages.Count();
+            Console.WriteLine();
+            Console.WriteLine($"нашел {imagesCollection.Count} картинок");
+            Console.WriteLine($"из них в {sberImages.Count()} встречается '{alt}'");
+            Console.WriteLine(sberImages.Any() ? "Тест пройден" : "Тест не пройден");
+            Console.WriteLine();
+            return sberImages.Count();
+        }
+    }
+}
